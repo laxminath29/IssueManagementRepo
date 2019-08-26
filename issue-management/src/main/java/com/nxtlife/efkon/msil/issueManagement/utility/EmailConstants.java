95cdd21d4c92890375b12224ef03429a5fefd8fb
laxmi <laxmi@LAPTOP-UP7LS920>
2019-08-26 14:00:57 +0530

    Constant fields for Email sending

---
+++ issue-management/src/main/java/com/nxtlife/efkon/msil/issueManagement/utility/EmailConstants.java
@@ -0,0 +1,10 @@
+package com.nxtlife.efkon.msil.issueManagement.utility;
+
+public interface EmailConstants {
+	
+    String FROM_EMAIL = "vtsefkon@gmail.com";
+ 
+     String FROM_PASSWORD = "vts@1234";
+ 
+     String TO_EMAIL = "laxmi.ssj4@gmail.com";
+}
