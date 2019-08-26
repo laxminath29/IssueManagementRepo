85a588d5c7b4b673945bc645aaf72d8fca3d3d85
laxmi <laxmi@LAPTOP-UP7LS920>
2019-08-26 14:08:27 +0530

    Endpoint to deal with Email sending

---
+++ issue-management/src/main/java/com/nxtlife/efkon/msil/issueManagement/controller/NotificationController.java
@@ -0,0 +1,31 @@
+package com.nxtlife.efkon.msil.issueManagement.controller;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nxtlife.efkon.msil.issueManagement.dto.MailRequest;
+import com.nxtlife.efkon.msil.issueManagement.dto.MailResponse;
+import com.nxtlife.efkon.msil.issueManagement.service.NotificationService;
+
+@RestController
+public class NotificationController {
+
+	@Autowired
+	private NotificationService notificationService;
+
+	@PostMapping("/sendingEmail")
+	public MailResponse sendEmail(@RequestBody MailRequest request)
+			throws AddressException, MessagingException, IOException {
+		notificationService.sendEmail(request);
+		return new MailResponse("Mail sent successfully", true);
+
+	}
+
+}
