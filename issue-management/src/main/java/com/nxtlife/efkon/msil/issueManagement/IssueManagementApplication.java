e4d2befc4d7def23f0fdf0ef357542f3988f6986
laxmi <laxmi@LAPTOP-UP7LS920>
2019-09-02 11:38:07 +0530

    Main Spring Boot Application class

--- issue-management/src/main/java/com/nxtlife/efkon/msil/issueManagement/IssueManagementApplication.java
+++ issue-management/src/main/java/com/nxtlife/efkon/msil/issueManagement/IssueManagementApplication.java
@@ -2,14 +2,15 @@ package com.nxtlife.efkon.msil.issueManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
 
 @SpringBootApplication
-//@ComponentScan(basePackages="com.nxtlife.efkon.msil.issueManagement")
+@EnableAsync
 public class IssueManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IssueManagementApplication.class, "--debug");
+		SpringApplication.run(IssueManagementApplication.class, args);
 	}
 
 }
